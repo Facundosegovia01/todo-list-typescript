ca361eb8a77102454ae273dc829d86fa8ece03d8
Facundo Segovia <fsegovia06@gmail.com>
2024-09-17 11:52:11 -0300

    solucionando errores de importacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Todos } from './components/Todos';
 import { FilterValue, type TodoId, type Todo as TodoType } from './types';
 import { TODO_FILTERS } from './consts';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { TodoTitle } from './types';
-
+ 
 const mockTodos = [{
   id: 1,
   title: 'todo 1',
@@ -22,7 +22,7 @@ const mockTodos = [{
   completed: false,
 }]
 const App = (): JSX.Element => {
-  const [todos, setTodos] = useState<TodoType[]>(mockTodos);
+  const [todos, setTodos] = useState (mockTodos);
  const [filterSelected, setFilterSelected] = useState<FilterValue>(TODO_FILTERS.ALL);
   const handleRemove = ({ id }: TodoId): void => {
     const newTodos = todos.filter(todo => todo.id !== id);
@@ -65,9 +65,8 @@ const App = (): JSX.Element => {
     title,
      id: crypto.randomUUID(),
      completed: false
-    };
+    }
 
-    
     const newTodos = [...todos, newTodo]
     setTodos(newTodos)
   }
